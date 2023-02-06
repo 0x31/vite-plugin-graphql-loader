966f8f768201a4529a75491990f769bfbb951583
noah-ren <gh@noiach.com>
2023-02-06 15:18:06 +1100

    Check plugin type in test

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,7 +1,12 @@
 import vitePluginGraphqlLoader from "../src";
 import { readFileSync, writeFileSync } from "fs";
+import { PluginOption } from "vite";
 
 const testcases = ["test1.gql", "test2.graphql"];
+
+// Check that the plugin abide by the PluginOption interface
+const plugin: PluginOption = vitePluginGraphqlLoader();
+console.assert(plugin !== undefined, "plugin should not be null");
 
 for (const testcase of testcases) {
     const fileContent = readFileSync(`__test__/testcases/${testcase}`, "utf-8");
